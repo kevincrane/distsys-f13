4debf86e90a717c65d319cb87c14d8274248b479
Prashanth Balasubramaniam <prashanth.bala9@gmail.com>
2013-09-06 17:18:20 -0400

    init java

---
+++ hw1_process_migration/src/ProcessManager.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Prashanth
+ * Date: 9/6/13
+ * Time: 5:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class ProcessManager {
+    public startProcess() {
+
+    }
+
+    public
+}
