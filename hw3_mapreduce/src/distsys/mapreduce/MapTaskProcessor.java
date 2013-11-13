de9e0feabc296c3a20af11ec60b63cac7ea324d3
Prashanth Balasubramaniam <prashanth.bala9@gmail.com>
2013-11-13 06:43:05 -0500

    DO THE MAP

--- hw3_mapreduce/src/distsys/mapreduce/MapTaskProcessor.java
+++ hw3_mapreduce/src/distsys/mapreduce/MapTaskProcessor.java
@@ -1,10 +1,14 @@
 package distsys.mapreduce;
 
 import distsys.Config;
+import distsys.kdfs.DistFile;
 import distsys.msg.CommHandler;
 import distsys.msg.TaskUpdateMessage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,17 +29,35 @@ public class MapTaskProcessor extends TaskProcessor {
         Mapper mapper = task.getMapper();
 
         System.out.println("Got Map Task in Slave: " + task);
-        // DO MAP
-        //TODO MAPPER JOB FILE IS NAMED SO IT CAN BE FETCHED BY JOBID  mpr_<JOBID>
-        //TODO HANDLE FAILURE BY SENDING MASTER NO MESSAGES OR BY SENDING ISrunning false, IsDONE false, message
 
-        // DONE MAP
+        // DO MAP
+        DistFile distFile = task.getDistFile();
+        Record<Integer, String> currentRecord;
+
+        while ((currentRecord = distFile.nextRecord()) != null) {
+            mapper.map(currentRecord.getKey(), currentRecord.getValue());
+        }
+
+        List<Record> mapOutput = mapper.getMapOutput();
+        File outputFile = new File(Config.MAP_RESULTS + task.getJobID());
+        boolean isDone = false;
+        try {
+            FileWriter fw = new FileWriter(outputFile);
+            for(Record record: mapOutput) {
+                fw.write(record.getKey() + "\t" + record.getValue() + "\n");
+            }
+            fw.close();
+            isDone = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            //Could not complete, the TaskUpdateMessage, will have isDone set to false as it should
+        }
 
         // We are DONE, TELL MASTER we are done, we retry in case of failure for MAX_SOCKET_TRIES times
         int maxTries = Config.MAX_SOCKET_TRIES;
         while (maxTries > 0) {
             try {
-                getMasterComm().sendMessage(new TaskUpdateMessage(task.getJobID(), false, true));
+                getMasterComm().sendMessage(new TaskUpdateMessage(task.getJobID(), false, isDone));
                 System.out.println("Sent message to master. Map Job with Id: " + task.getJobID() + " is done.");
                 break;
             } catch (IOException e) {
